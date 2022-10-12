efa3adcf787f8178db46885e1e36eb92637bb995
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-10-13 05:51:58 +0700

    fix ui edit anggota

--- app/Views/admin/editAnggota.php
+++ app/Views/admin/editAnggota.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>SIHIMA</title>
@@ -29,144 +30,148 @@
 
     <!-- Template Stylesheet -->
     <link href="/assets/css/style.css" rel="stylesheet">
-    </head>
+</head>
 
-    <body>
-        <div class="container-fluid position-relative d-flex p-0">
-            <!-- Sidebar Start -->
-            <div class="sidebar pe-4 py-3">
-                <nav class="navbar bg-secondary navbar-dark">
-                    <div class="d-flex align-items-center ms-4 mb-4">
-                        <div class="position-relative">
-                            <img class="rounded-circle mb-2" src="/assets/img/himakom.png" style="height: 30px;">
-                        </div>
-                        <a href="">
-                            <h3 class="ff-gantari ms-2"></i>SIHIMA</h3>
-                        </a>
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+        <!-- Sidebar Start -->
+        <div class="sidebar pe-4 py-3">
+            <nav class="navbar bg-secondary navbar-dark">
+                <div class="d-flex align-items-center ms-4 mb-4">
+                    <div class="position-relative">
+                        <img class="rounded-circle mb-2" src="/assets/img/himakom.png" style="height: 30px;">
                     </div>
-                    <div class="d-flex align-items-center ms-4 mt-2 mb-4">
-                        <div class="position-relative">
-                            <img class="rounded-circle" src="/assets/img/cat.jpg" style="width: 40px; height: 40px;">
-                            <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0 ff-gantari">Kucing</h6>
-                            <span class="ff-gantari">Admin</span>
-                        </div>
-                    </div>
-
-                    <div class="navbar-nav w-100">
-                        <a href="" class="nav-item nav-link active ff-gantari"><i class="fa fa-table me-2"></i>Tabel</a>
-                    </div>
-                </nav>
-            </div>
-            <!-- Sidebar End -->
-
-            <!-- Content Start -->
-            <div class="content">
-                <!-- Navbar Start -->
-                <nav class="navbar navbar-expand bg-dark navbar-dark sticky-top px-4 py-3">
-                    <a href="" class="navbar-brand d-flex d-lg-none me-4">
+                    <a href="">
+                        <h3 class="ff-gantari ms-2"></i>SIHIMA</h3>
                     </a>
-                    <a href="#" class="sidebar-toggler flex-shrink-0">
-                        <i class="fa fa-bars"></i>
-                    </a>
-                    <form class="d-none d-md-flex ms-4">
-                        <input class="form-control bg-dark border-0 ff-gantari" type="search" placeholder="Search">
-                    </form>
-                    <div class="navbar-nav align-items-center ms-auto">
+                </div>
+                <div class="d-flex align-items-center ms-4 mt-2 mb-4">
+                    <div class="position-relative">
+                        <img class="rounded-circle" src="/assets/img/cat.jpg" style="width: 40px; height: 40px;">
+                        <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
-                </nav>
-                <!-- Navbar End -->
+                    <div class="ms-3">
+                        <h6 class="mb-0 ff-gantari">Kucing</h6>
+                        <span class="ff-gantari">Admin</span>
+                    </div>
+                </div>
 
-                <div class="container-fluid pt-4 px-4">
-                    <div class="row g-4">
-                        <div class="col-12">
-                            <div class="bg-secondary rounded h-100 p-4">
-                                <h5 class="mb-4 ff-gantari">Edit Anggota</h5>
-                                <form action="/update/<?= $Biodata['id_anggota'] ?>" method="post">
-                                    <div class="form-floating border-0 mb-3 ff-gantari">
-                                        <input type="text" class="form-control text-light" id="nama_anggota" name="nama_anggota" value="<?= $Biodata['nama_anggota'] ?>">
-                                        <label for="nama_anggota">Nama Lengkap</label>
-                                    </div>
-                                    <div class="form-floating border-0 mb-3 ff-gantari">
-                                        <input type="text" class="form-control text-light" id="npm" value="<?= $Biodata['npm'] ?>">
-                                        <label for="npm">NPM</label>
-                                    </div>
-                                    <div class="form-floating border-0 mb-3 ff-gantari">
-                                        <select class="form-select text-light" id="bidang" aria-label="Bidang">
+                <div class="navbar-nav w-100">
+                    <a href="" class="nav-item nav-link active ff-gantari"><i class="fa fa-table me-2"></i>Tabel</a>
+                </div>
+            </nav>
+        </div>
+        <!-- Sidebar End -->
+
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+            <nav class="navbar navbar-expand bg-dark navbar-dark sticky-top px-4 py-3">
+                <a href="" class="navbar-brand d-flex d-lg-none me-4">
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <form class="d-none d-md-flex ms-4">
+                    <input class="form-control bg-dark border-0 ff-gantari" type="search" placeholder="Search">
+                </form>
+                <div class="navbar-nav align-items-center ms-auto">
+                </div>
+            </nav>
+            <!-- Navbar End -->
+
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-12">
+                        <div class="bg-secondary rounded h-100 p-4">
+                            <h5 class="mb-4 ff-gantari">Edit Anggota</h5>
+                            <form action="/update/<?= $Biodata['id_anggota'] ?>" method="post">
+                                <div class="form-floating border-0 mb-3 ff-gantari">
+                                    <input type="text" class="form-control text-light" id="nama_anggota" name="nama_anggota" value="<?= $Biodata['nama_anggota'] ?>">
+                                    <label for="nama_anggota">Nama Lengkap</label>
+                                </div>
+                                <div class="form-floating border-0 mb-3 ff-gantari">
+                                    <input type="text" class="form-control text-light" id="npm" value="<?= $Biodata['npm'] ?>">
+                                    <label for="npm">NPM</label>
+                                </div>
+                                <div class="form-floating border-0 mb-3 ff-gantari">
+                                    <select class="form-select text-light" id="bidang" aria-label="Bidang">
+                                        <?php foreach ($Bidang as $bidang) : ?>
                                             <?php if ($bidang['id_bidang'] == $Biodata['id_bidang']) : ?>
                                                 <option selected value="<?= $bidang['id_bidang'] ?>"><?= $bidang['nama_bidang'] ?></option>
                                             <?php else : ?>
                                                 <option value="<?= $bidang['id_bidang'] ?>"><?= $bidang['nama_bidang'] ?></option>
                                             <?php endif ?>
-                                        </select>
-                                        <label for="bidang">Bidang</label>
-                                    </div>
-                                    <div class="form-floating border-0 mb-3 ff-gantari">
-                                        <select class="form-select text-light" id="prodi" aria-label="Prodi">
+                                        <?php endforeach; ?>
+                                    </select>
+                                    <label for="bidang">Bidang</label>
+                                </div>
+                                <div class="form-floating border-0 mb-3 ff-gantari">
+                                    <select class="form-select text-light" id="prodi" aria-label="Prodi">
+                                        <?php foreach ($Prodi as $prodi) : ?>
                                             <?php if ($prodi['id_prodi'] == $Biodata['id_prodi']) : ?>
                                                 <option selected value="<?= $prodi['id_prodi'] ?>"><?= $prodi['nama_prodi'] ?></option>
                                             <?php else : ?>
                                                 <option value="<?= $prodi['id_prodi'] ?>"><?= $prodi['nama_prodi'] ?></option>
                                             <?php endif ?>
-                                        </select>
-                                        <label for="prodi">Prodi</label>
-                                    </div>
-                                    <div class="form-floating border-0 mb-3 ff-gantari">
-                                        <select class="form-select text-light" id="status_jabatan" aria-label="Status Jabatan">
-                                            <?php foreach ($Jabatan as $jabatan) : ?>
-                                                <?php if ($jabatan['id_jabatan'] == $Biodata['id_jabatan']) : ?>
-                                                    <option selected value="<?= $jabatan['id_jabatan'] ?>"><?= $jabatan['nama_jabatan'] ?></option>
-                                                <?php else : ?>
-                                                    <option value="<?= $jabatan['id_jabatan'] ?>"><?= $jabatan['nama_jabatan'] ?></option>
-                                                <?php endif ?>
-                                            <?php endforeach; ?>
-                                        </select>
-                                        <label for="status_jabatan">Prodi</label>
-                                    </div>
-                                    <div class="form-floating border-0 mb-3 ff-gantari">
-                                        <input type="text" class="form-control text-light" id="periode" name="periode" value="<?= $Biodata['periode'] ?>">
-                                        <label for="periode">Periode</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-dark ff-gantari"><i class="fa fa-paper-plane"></i> Submit</button>
-                                </form>
-                            </div>
+                                        <?php endforeach; ?>
+                                    </select>
+                                    <label for="prodi">Prodi</label>
+                                </div>
+                                <div class="form-floating border-0 mb-3 ff-gantari">
+                                    <select class="form-select text-light" id="status_jabatan" aria-label="Status Jabatan">
+                                        <?php foreach ($Jabatan as $jabatan) : ?>
+                                            <?php if ($jabatan['id_jabatan'] == $Biodata['id_jabatan']) : ?>
+                                                <option selected value="<?= $jabatan['id_jabatan'] ?>"><?= $jabatan['nama_jabatan'] ?></option>
+                                            <?php else : ?>
+                                                <option value="<?= $jabatan['id_jabatan'] ?>"><?= $jabatan['nama_jabatan'] ?></option>
+                                            <?php endif ?>
+                                        <?php endforeach; ?>
+                                    </select>
+                                    <label for="status_jabatan">Prodi</label>
+                                </div>
+                                <div class="form-floating border-0 mb-3 ff-gantari">
+                                    <input type="text" class="form-control text-light" id="periode" name="periode" value="<?= $Biodata['periode'] ?>">
+                                    <label for="periode">Periode</label>
+                                </div>
+                                <button type="submit" class="btn btn-dark ff-gantari"><i class="fa fa-paper-plane"></i> Submit</button>
+                            </form>
                         </div>
+                    </div>
 
-                        <!-- Footer Start -->
-                        <footer class="container-fluid pt-4 px-4">
-                            <div class="bg-secondary rounded-top p-4">
-                                <div class="row">
-                                    <div class="col-12 col-sm-6 text-center text-sm-start text-tertiary ff-gantari">
-                                        &copy; 2022 <a href="#">SIHIMA</a>, All Right Reserved.
-                                    </div>
+                    <!-- Footer Start -->
+                    <footer class="container-fluid pt-4 px-4">
+                        <div class="bg-secondary rounded-top p-4">
+                            <div class="row">
+                                <div class="col-12 col-sm-6 text-center text-sm-start text-tertiary ff-gantari">
+                                    &copy; 2022 <a href="#">SIHIMA</a>, All Right Reserved.
                                 </div>
                             </div>
-                        </footer>
-                        <!-- Footer End -->
-                    </div>
+                        </div>
+                    </footer>
+                    <!-- Footer End -->
                 </div>
             </div>
-            <!-- Content End -->
-
-
-            <!-- Back to Top -->
-            <a href="\tambah" class="btn btn-lg btn-light btn-lg-square back-to-top"><i class="bi bi-plus text-dark"></i></a>
         </div>
+        <!-- Content End -->
 
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="lib/chart/chart.min.js"></script>
-        <script src="lib/easing/easing.min.js"></script>
-        <script src="lib/waypoints/waypoints.min.js"></script>
-        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-        <script src="lib/tempusdominus/js/moment.min.js"></script>
-        <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
 
-        <!-- Template Javascript -->
-        <script src="js/main.js"></script>
-    </body>
+        <!-- Back to Top -->
+        <a href="\tambah" class="btn btn-lg btn-light btn-lg-square back-to-top"><i class="bi bi-plus text-dark"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/chart/chart.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
 
 </html>

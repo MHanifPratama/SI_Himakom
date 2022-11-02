9e23fa1c3713347b260d492f9b641c92c411ac75
MHanifPratama <mhanifp27@gmail.com>
2022-11-03 01:19:34 +0700

    seeder

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,40 @@ class Home extends BaseController
 {
     public function index()
     {
+<<<<<<< Updated upstream
         return view('admin\list');
+=======
+        $model = new Bidang();
+        $modelBiodata = new Biodata();
+        $modelProgja = new Kepanitiaan();
+        $dataProgja = $modelProgja->join('bidang','bidang.id_bidang=kepanitiaan.id_bidang')->get()->getResultArray();;
+        $modelBidang = new Bidang();
+        // $data['bidang'] = $model->findAll();
+        $data = [
+            'title' => 'Home',
+            'bidang' => $model->findAll(),
+            'totalAnggota'=> $modelBiodata->countAllResults(),
+            'totalProgja'=> $modelProgja->countAllResults(),
+            'totalBidang'=> $modelBidang->countAllResults(),
+            'progja' => $dataProgja
+        ];
+        // echo var_dump($data['totalProgja']);
+        return view('home\home',$data);
+    }
+    public function viewKeanggotaan(){
+        return view('home\cariKeAnggotaan');
+    }
+    public function cariKeanggotaan(){
+        $modelBiodata = new Biodata();
+        $hasil = $modelBiodata->join('bidang','bidang.id_bidang = biodata.id_bidang')
+        ->join('prodi','prodi.id_prodi =biodata.id_prodi')
+        ->join('jabatan','jabatan.id_jabatan = biodata.id_jabatan')->like('biodata.npm', $this->request->getVar('npm'))->findAll();
+        // echo var_dump($hasil);
+        $data = [
+            'title' => 'Keanggotaan',
+            'hasil' => $hasil,
+        ];
+        return view('home\hasilPencarian',$data);
+>>>>>>> Stashed changes
     }
 }
